93788ac0fb83f4059e7becbc8c77c348c522065c
Rab-son <rabsonsayendajnr@gmail.com>
2022-12-29 06:48:49 +0200

    register route

--- laravel-sanctum-api/routes/api.php
+++ laravel-sanctum-api/routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 //Route::resource('products', ProductController::class);
 
 // Public routes
-Route::post('/register',[AuthController::class, 'store']);
+Route::post('/register',[AuthController::class, 'register']);
 Route::get('/products',[ProductController::class, 'index']);
 Route::get('/products/{id}',[ProductController::class, 'show']);
 Route::get('products/search/{name}', [ProductController::class, 'search']);
