e893cf30e20cb1a5220ca3361e021a2e2cc5689f
Rab-son <rabsonsayendajnr@gmail.com>
2022-12-29 06:25:55 +0200

    protecting routes

--- laravel-sanctum-api/routes/api.php
+++ laravel-sanctum-api/routes/api.php
@@ -22,8 +22,14 @@ Route::post('/products',[ProductController::class, 'store']);
 
 Route::resource('products', ProductController::class);
 
-Route::get('products/search/{name}', [ProductController::class, 'search']);
 
+
+Route::middleware('auth:sanctum')->get('/user', function () {
+    Route::get('products/search/{name}', [ProductController::class, 'search']);
+});
+
+/*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
+
