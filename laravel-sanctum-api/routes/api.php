c7deb193a386dc322ce9522aec9f01a988756696
Rab-son <rabsonsayendajnr@gmail.com>
2022-12-29 06:46:26 +0200

    auth controller

--- laravel-sanctum-api/routes/api.php
+++ laravel-sanctum-api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 //Route::resource('products', ProductController::class);
 
 // Public routes
+Route::post('/register',[AuthController::class, 'store']);
 Route::get('/products',[ProductController::class, 'index']);
 Route::get('/products/{id}',[ProductController::class, 'show']);
 Route::get('products/search/{name}', [ProductController::class, 'search']);
